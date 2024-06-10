48b220cad831b73279ab66f3dcf60dd5f1f50104
Sakib <ssar96540@gmail.com>
2024-06-10 11:19:34 +0600

    post sumbission detail => collection name submissionCollection

--- index.js
+++ index.js
@@ -8,10 +8,10 @@ require('dotenv').config()
 // console.log(stripe,'secrete');
 // middleware
 app.use(cors({
-  origin:[
-    'http://localhost:5173',  
-   'https://taskmania-85588.web.app'
-  ], 
+  origin: [
+    'http://localhost:5173',
+    'https://taskmania-85588.web.app'
+  ],
 }));
 app.use(express.json())
 
@@ -36,6 +36,7 @@ const userCollection = client.db('TaskMania').collection('Users')
 const taskCollection = client.db('TaskMania').collection('Tasks')
 const purchaseCoinCollection = client.db('TaskMania').collection('PurchaseCoin')
 const paymentCoinCollection = client.db('TaskMania').collection('payment')
+const submissionCollection = client.db('TaskMania').collection('Submission')
 async function run() {
   try {
 
@@ -51,6 +52,27 @@ async function run() {
       const result = await userCollection.insertOne(user)
       res.send(result)
     })
+    // update user coin 
+    app.put('/user', async(req,res)=>{
+      const cn = req.body;
+      const email = cn.email;
+      const coin = cn.coin;
+      
+      const query = { email: email };
+      const user = await userCollection.findOne(query);
+      if(user){
+        const newCoinValue = (user.coin || 0) + coin;
+
+        const newUp = {
+          $set: {
+             coin:newCoinValue 
+          }
+        }
+        const result = await userCollection.updateOne(query, newUp)
+        res.send(result)
+      }
+      console.log(coin,'update this coin in the database');
+    })
     // get specific user
     app.get('/user/:email', async (req, res) => {
       const email = req.params.email;
@@ -63,90 +85,113 @@ async function run() {
     // post task 
     app.post('/task', async (req, res) => {
       const task = req.body;
+      const { quantity, amount, email } = task;
+      const qantity = parseInt(quantity);
+      const amnt = parseInt(amount);
+      const totalCost = qantity * amnt;
+      const creator = await userCollection.findOne({ email: email })
+      if (creator.coin < totalCost) {
+        return res.status(400).json({ message: "Not enough coins. Please purchase more coins." });
+      }
+      console.log(creator);
       const result = await taskCollection.insertOne(task)
-      console.log(task);
       res.send(result)
     })
-// get all task 
-// /tasks
-// GET
-// It will send all documents from  taskCollection where the taskCount is greater than 0  from Database.   ( use $gt ) 
+    // get all task 
+    // /tasks
+    // GET
+    // It will send all documents from  taskCollection where the taskCount is greater than 0  from Database.   ( use $gt ) 
     app.get('/tasks', async (req, res) => {
       const tasks = await taskCollection.find().toArray();
       res.send(tasks)
     })
-//  my tasks 
-app.get('/tasks/:email', async(req,res)=>{
-  const email = req.params.email;
-  const query = {email : email }
-  // date time diya sort hocce na .
-  const result = await taskCollection.find(query).sort({date : -1 }).toArray();
-  console.log(email,'hah');
-  res.send(result)
-})
-// delete tasks
-// ● onClicking Delete, delete the task from task Collection. And Increase the
-// (task_quantity* payable_amount) coin in his available coin
+    //  my tasks 
+    app.get('/tasks/:email', async (req, res) => {
+      const email = req.params.email;
+      const query = { email: email }
+      // date time diya sort hocce na .
+      const result = await taskCollection.find(query).sort({ date: -1 }).toArray();
+      console.log(email, 'hah');
+      res.send(result)
+    })
+    // get id based tsk for show task detail => worker
+    app.get('/task/:id', async(req,res)=>{
+      const id = req.params.id;
+      const query = {_id:new ObjectId(id)}
+      const result = await taskCollection.findOne(query)
+      res.send(result)
+      console.log(id,'id from worker detail');
+    })
+    // post submission details
+    app.post('/submission',async(req,res)=>{
+      const submit = req.body;
+      console.log(submit);
+      const result = await submissionCollection.insertOne(submit)
+      res.send(result)
+    })
+    // delete tasks
+    // ● onClicking Delete, delete the task from task Collection. And Increase the
+    // (task_quantity* payable_amount) coin in his available coin
 
-app.delete('/task/:id', async(req,res)=>{
-  const id = req.params.id;
-  const query = {_id : new ObjectId(id)}
-  const result = await taskCollection.deleteOne(query)
-  console.log(id);
-  res.send(result);
-})
-//  update tasks 
- app.put('/task/update/:id',async(req,res)=> {
-  const update = req.body;
-  const id = req.params.id;
-  const query = {_id : new ObjectId(id)}
-  const newUp = {
-   $set:{
-     ...update,
-   }
-  }
-  const result = await taskCollection.updateOne(query, newUp)
-  res.send(result)
-})
-// get all purchase data
-app.get('/purchaseCoin',async(req,res)=>{
-  const result = await purchaseCoinCollection.find().toArray()
-  res.send(result)
-})
+    app.delete('/task/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const result = await taskCollection.deleteOne(query)
+      console.log(id);
+      res.send(result);
+    })
+    //  update tasks 
+    app.put('/task/update/:id', async (req, res) => {
+      const update = req.body;
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const newUp = {
+        $set: {
+          ...update,
+        }
+      }
+      const result = await taskCollection.updateOne(query, newUp)
+      res.send(result)
+    })
+    // get all purchase data
+    app.get('/purchaseCoin', async (req, res) => {
+      const result = await purchaseCoinCollection.find().toArray()
+      res.send(result)
+    })
 
-// payment intent 
-app.post('/create-payment-intent', async(req,res)=>{
-  const {coin} = req.body;
-console.log(coin);  
-  // Check if coin is a valid number
-  if(isNaN(coin) || coin <= 0) {
-    return res.status(400).send({ error: 'Invalid coin amount' });
-  }
-  const amount = parseInt(coin * 100);
-  console.log(amount, 'inside the intent ');
+    // payment intent 
+    app.post('/create-payment-intent', async (req, res) => {
+      const { coin } = req.body;
+      console.log(coin);
+      // Check if coin is a valid number
+      if (isNaN(coin) || coin <= 0) {
+        return res.status(400).send({ error: 'Invalid coin amount' });
+      }
+      const amount = parseInt(coin * 100);
+      console.log(amount, 'inside the intent ');
 
-  const paymentIntent = await stripe.paymentIntents.create({
-    amount: amount,
-    currency: "usd",
-    payment_method_types:['card']
-  });
-  
-  res.send({
-    clientSecret : paymentIntent.client_secret
-  });
-});
-// post payment data
-app.post('/payment',async(req,res)=>{
-  const payment = req.body;
-  console.log(payment);
-  const result = await paymentCoinCollection.insertOne(payment)
-  res.send(result)
-})
-// get all payment data 
-app.get('/payment', async(req,res)=> {
-  const result = await paymentCoinCollection.find().toArray();
-  res.send(result);
-})
+      const paymentIntent = await stripe.paymentIntents.create({
+        amount: amount,
+        currency: "usd",
+        payment_method_types: ['card']
+      });
+
+      res.send({
+        clientSecret: paymentIntent.client_secret
+      });
+    });
+    // post payment data
+    app.post('/payment', async (req, res) => {
+      const payment = req.body;
+      console.log(payment);
+      const result = await paymentCoinCollection.insertOne(payment)
+      res.send(result)
+    })
+    // get all payment data 
+    app.get('/payment', async (req, res) => {
+      const result = await paymentCoinCollection.find().toArray();
+      res.send(result);
+    })
 
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
